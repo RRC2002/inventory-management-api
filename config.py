1a5ddf6b41b8b11ebdd9061f1d1c65bec1b004a1
Rajashree Roy Choudhury <rajashreeroyc@gmail.com>
2026-06-25 11:32:10 +0530

    Connected Railway database

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:mysql%4006%2Fbts@localhost/inventory_db"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+mysqlconnector://{os.getenv('MYSQLUSER')}:"
+        f"{os.getenv('MYSQLPASSWORD')}@"
+        f"{os.getenv('MYSQLHOST')}:"
+        f"{os.getenv('MYSQLPORT')}/"
+        f"{os.getenv('MYSQLDATABASE')}"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
